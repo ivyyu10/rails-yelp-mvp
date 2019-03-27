1d519fd9e90eaf8096e88d6c1f571858a1a30d72
Ivy Yu <ivys.yu@gmail.com>
2019-03-27 16:09:49 +0800

    yelp MVP rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.delete_all
+
+10.times do
+  Restaurant.create!(name: Faker::Restaurant.name, address: Faker::Address.street_address, category: ["chinese", "italian", "japanese", "french", "belgian"].sample)
+end
+puts "Created #{Restaurant.count} restaurants!"
